e89a767325923f54b5ed2f12a4b22f6ae5f07a1f
LGunay <gqasimova93@gmail.com>
2024-04-23 22:34:47 +0400

    added username

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -9,14 +9,12 @@ if __name__ == '__main__':
     userid = sys.argv[1]
     data = requests\
         .get(f'https://jsonplaceholder.typicode.com/users/{userid}')\
-        .json().get('name')
+        .json().get('username')
     todos = requests\
         .get(f'https://jsonplaceholder.typicode.com/users/{userid}/todos')\
         .json()
-    dat = ''
-    for tasks in todos:
-        status = tasks.get("completed")
-        title = tasks.get("title")
-        dat += f'"{userid}","{data}","{status}","{title}"\n'
-    with open(f'{userid}.csv', 'w') as f:
-        f.write(dat)
+    with open(f"{userid}.csv", "a") as f:
+        for tasks in todos:
+            status = tasks.get("completed")
+            title = tasks.get("title")
+            f.write(f'"{userid}","{data}","{status}","{title}"\n')
