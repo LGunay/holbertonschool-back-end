30113252238888261d811f93c9c03fd297c990f2
LGunay <gqasimova93@gmail.com>
2024-04-23 18:48:31 +0400

    argv added

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -11,15 +11,15 @@ if __name__ == '__main__':
     TASK_TITLE = []
     data = requests\
         .get(f'https://jsonplaceholder.typicode.com/users/{userid}')\
-        .json()['name']
+        .json().get('name')
     todos = requests\
         .get(f'https://jsonplaceholder.typicode.com/users/{userid}/todos')\
         .json()
     TOTAL_NUMBER_OF_TASKS = len(todos)
     for tasks in todos:
-        if tasks['completed']:
+        if tasks.get('completed'):
             NUMBER_OF_DONE_TASKS += 1
-            TASK_TITLE.append(tasks['title'])
+            TASK_TITLE.append(tasks.get('title'))
     print(f'Employee {data} is done with tasks'
           f'({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):')
     for title in TASK_TITLE:
