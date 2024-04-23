0b55c9dcfa92d052ea572cc907dc24c2def4a2a1
LGunay <gqasimova93@gmail.com>
2024-04-23 18:32:41 +0400

    added main

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -2,19 +2,20 @@
 
 import requests
 
-userid = input()
-NUMBER_OF_DONE_TASKS = 0
-TASK_TITLE = []
-data = requests.get(f'https://jsonplaceholder.typicode.com/users/{userid}').\
-    json()['name']
-todos = requests.get(f'https://jsonplaceholder.typicode.com/users/\
-                     {userid}/TODOS').json()
-TOTAL_NUMBER_OF_TASKS = len(todos)
-for tasks in todos:
-    if tasks['completed']:
-        NUMBER_OF_DONE_TASKS += 1
-        TASK_TITLE.append(tasks['title'])
-print(f'Employee {data} is done with tasks\
-      ({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):')
-for title in TASK_TITLE:
-    print("\t "+title)
+if __name__ == '__main__':
+    userid = input()
+    NUMBER_OF_DONE_TASKS = 0
+    TASK_TITLE = []
+    data = requests.get(f'https://jsonplaceholder.typicode.com/users/\
+                        {userid}').json()['name']
+    todos = requests.get(f'https://jsonplaceholder.typicode.com/users/\
+                         {userid}/TODOS').json()
+    TOTAL_NUMBER_OF_TASKS = len(todos)
+    for tasks in todos:
+        if tasks['completed']:
+            NUMBER_OF_DONE_TASKS += 1
+            TASK_TITLE.append(tasks['title'])
+    print(f'Employee {data} is done with tasks\
+          ({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):')
+    for title in TASK_TITLE:
+        print("\t "+title)
