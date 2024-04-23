e6b38c57fafb7a38ab92299f61020f971352fc96
LGunay <gqasimova93@gmail.com>
2024-04-23 22:24:31 +0400

    1-export_to_CSV.py

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Starting with API
+"""
+
+if __name__ == '__main__':
+    import requests
+    import sys
+    userid = sys.argv[1]
+    data = requests\
+        .get(f'https://jsonplaceholder.typicode.com/users/{userid}')\
+        .json().get('name')
+    todos = requests\
+        .get(f'https://jsonplaceholder.typicode.com/users/{userid}/todos')\
+        .json()
+    with open(f"{userid}.csv", "a") as f:
+        for tasks in todos:
+            status = tasks.get("completed")
+            title = tasks.get("title")
+            f.write(f'"{userid}","{data}","{status}","{title}"\n')
